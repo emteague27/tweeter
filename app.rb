9ac6400f7fae8c23d053de15a7a7a37250993319
Erin <eteague27@gmail.com>
2016-01-04 16:43:02 -0500

    added gets to app.rb

--- app.rb
+++ app.rb
@@ -5,3 +5,26 @@ require "./models.rb"
 
 set :database, "sqlite3:tweeterdb.sqlite3"
 
+get '/' do 
+	erb :'sign-in'
+end
+
+get '/home' do
+	erb :home
+end
+
+get '/profile/:id' do
+	erb :profile
+end
+
+get '/account' do
+	erb :account
+end
+
+get '/new-post' do
+	erb :'new-post'
+end
+
+get '/following' do
+	erb :following
+end
