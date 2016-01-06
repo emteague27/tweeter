ace1b6a129d4cb663b3b2aa4ff8bc7c7a7c12f53
Christine Chan <Chan-Christine@hotmail.com>
2016-01-06 17:11:48 -0500

    updated tweeterdb.sqlite3

--- public/main.js
+++ public/main.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 	$(".sly").hide();
 	$(".params").hide();
 	$(".param3").hide();
-		$(".bird").fadeOut(4000);
-		$(".param").fadeOut(4100);
-			$(".sly").fadeIn(4200);
-			$(".params").fadeIn(4200);
-			$(".param3").fadeIn(4200);
+		$(".bird").fadeOut(3200);
+		$(".param").fadeOut(3300);
+			$(".sly").fadeIn(3400);
+			$(".params").fadeIn(3400);
+			$(".param3").fadeIn(3400);
 });
