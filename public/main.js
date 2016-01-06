f487f004f39c34497c6cd46cdd9b8a3e802ea287
Christine Chan <Chan-Christine@hotmail.com>
2016-01-06 15:52:02 -0500

    modified error page and jquery page

--- public/main.js
+++ public/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 	$(".sly").hide();
-	$("#params").hide();
+	$(".params").hide();
+	$(".param3").hide();
 		$(".bird").fadeOut(4000);
-		$("#param").fadeOut(4100);
+		$(".param").fadeOut(4100);
 			$(".sly").fadeIn(4200);
-			$("#params").fadeIn(4200);
+			$(".params").fadeIn(4200);
+			$(".param3").fadeIn(4200);
 });
