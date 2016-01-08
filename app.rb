ed2e64125ca5b28b3108e9849ad513cea41b9818
Willis Lau <lauwillis@yahoo.com>
2016-01-07 19:37:56 -0500

    profile page added to keep track of each user's posts

--- app.rb
+++ app.rb
@@ -5,15 +5,21 @@ require "./models.rb"
 
 set :database, "sqlite3:tweeterdb.sqlite3"
 
+enable :sessions
+
 get '/' do 
-	erb :'sign-in'
+	erb :'sign-in', :layout => false
 end
 
 get '/home' do
+	@user = current_user
+	@posts = Post.all.reverse
 	erb :home
 end
 
 get '/profile/:id' do
+	@user = current_user
+	@posts = Post.where(user_id: @user.id)
 	erb :profile
 end
 
@@ -29,6 +35,20 @@ get '/following' do
 	erb :following
 end
 
+get '/edit_account' do
+	@user = current_user
+	erb :'edit-account'
+end
+
+get '/delete' do
+	erb :'delete-account'
+end
+
+get '/logout' do
+	session.clear
+	redirect '/'
+end
+
 post '/sign-in' do
 	@user = User.where(username: params[:username]).last
 	if @user && @user.password == params[:password]
@@ -36,11 +56,42 @@ post '/sign-in' do
 		flash[:notice] = "You have signed in"
 		redirect '/home'
 	else
-		flash[:notice] = "I thought I thaw a puddytat"
-		redirect '/'
+		# flash[:notice] = "\"I thawt I taw a puddy tat!\" Sylvester says your username or password is incorrect."
+		redirect '/error'
 	end
 end
 
 post '/sign-up' do
+	@user = User.create(fname: params[:fname], lname: params[:lname], username: params[:username], password: params[:password], email: params[:email], birthdate: params[:birthdate])
+	session[:user_id] = @user.id
+	redirect '/home'
+end
 
+post '/edit_account' do
+	@user = current_user
+	@user.update(fname: params[:fname], lname: params[:lname], username: params[:username], password: params[:password], email: params[:email], birthdate: params[:birthdate])
+	redirect '/account'
+end
+
+def current_user
+	if session[:user_id]
+		@current_user = User.find(session[:user_id])
+	end
+end
+
+post '/twit' do
+	@posts = Post.new(title: params[:title], body: params[:text], user_id: current_user.id)
+	@posts.save
+	redirect '/home'
+end
+
+post '/delete' do
+	@user = current_user
+	@user.delete
+	session.clear
+	redirect '/'
+end
+
+get '/error' do
+	erb :'error'
 end
